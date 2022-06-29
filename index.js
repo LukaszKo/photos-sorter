397da417fe2c389438b776284e6de0a526ddfed6
Łukasz Kochajewski <lkochajewski@pgs-soft.com>
2022-06-29 12:22:46 +0200

    add accepted files extensions

--- index.js
+++ index.js
@@ -5,29 +5,23 @@ import moment from 'moment';
 const currentFolder = 'ala zdjecie telefon';
 const directoryPath = `/Users/lkochajewski/Desktop/${currentFolder}`;
 const files = fs.readdirSync(directoryPath, { encoding: 'utf-8' });
+const acceptedFilesExtensions = ['.jpg', '.mp4'];
 
 function start() {
 	let fileCounter = 0;
-	const imagesAndVideosFiles = files.filter(
-		(f) =>
-			path.extname(f).toLocaleLowerCase() === '.jpg' ||
-			path.extname(f).toLocaleLowerCase() === '.mp4'
+	const imagesAndVideosFiles = files.filter((f) =>
+		acceptedFilesExtensions.includes(path.extname(f).toLocaleLowerCase())
 	);
 	console.log(`START MOVING ${imagesAndVideosFiles.length} FILES`);
-
 	imagesAndVideosFiles.forEach((file, index) => {
 		const data = fs.statSync(directoryPath + '/' + file);
-
 		const month = moment(data.mtime).format('MM-YYYY');
-
 		const monthDirectory = directoryPath + '/' + month;
-
 		const isMonthDirExists = fs.existsSync(monthDirectory);
 		if (!isMonthDirExists) {
 			fs.mkdirSync(monthDirectory);
 			console.log(`NEW FOLDER ${monthDirectory} CREATED`);
 		}
-
 		fs.renameSync(`${directoryPath}/${file}`, `${monthDirectory}/${file}`);
 		fileCounter++;
 	});
